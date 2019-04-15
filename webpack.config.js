1d800a67e9b3a83d56c2eaa92d3a6e6fa9b5fc06
Redmond Tran <RedmondTran@builddirect.com>
2019-04-15 16:32:55 -0700

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.tpl$/,
+                use: 'raw-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Backbonejs',
+            template: 'src/index.html'
+        })
+    ],
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    }
+};
